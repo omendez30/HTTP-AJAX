da7e807176639501ce75ad5a51449a8d2ecff92d
omendez30 <omendez9@outlook.com>
2019-04-02 21:57:35 -0400

    added friends list and friend input

---
+++ friends/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Friends from "./components/Friends/Friends";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res =>
+        this.setState({ friends: res.data }, () =>
+          console.log(this.state.friends)
+        )
+      )
+      .catch(err => {
+        throw new Error(err);
+      });
+  }
+
+  handleUpdateList = friends => {
+    this.setState({ friends });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Friends friends={this.state.friends} />
+      </div>
+    );
+  }
+}
+
+export default App;
